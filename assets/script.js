b52bf119eb9041491df2b29229b59a952d30e902
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-06-04 12:19:38 -0500

    updated Javascript to rin a function

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,26 @@
 var topic = ['HTML', 'CSS', 'Git', 'Javascript'];
+var randomTopic = topic[Math.floor(Math.random() * topic.length)];
 
-for(var x = 0; x < topic.length; x++){
-    console.log(topic[x]);
+function listTopic() {
+    for (var x = 0; x < topic.length; x++) {
+      console.log(topic[x]);
+    }
+  } 
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+      console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+      console.log("Let's study CSS!");
+    } else if (randomTopic === 'Git') {
+      console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+      console.log("Let's study JavaScript!");
+    } else {
+      console.log('Please try again!');
 }
-/*
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
 }
-*/
+
+console.log('Here are the topics we learned through Prework:');
+listTopic();
+console.log('Which topic should we study first?');
+selectTopic();
